f523001717e94f44d39040f194063a2e22b29c58
Gustavokvs <gustavokruger514@gmail.com>
2024-08-05 20:04:13 -0300

    Completando

--- Aula4/src/br/ulbra/aula4/Jogador.java
+++ Aula4/src/br/ulbra/aula4/Jogador.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ulbra.aula4;
 
-/**
- *
- * @author gusta
- */
+import java.util.Scanner;
+
 public class Jogador {
+    
+    public static void main(String[] args) {
+        
+        RegistroJogador obj1 = new RegistroJogador();
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Qual é o jogador?");
+        obj1.setNome(ler.nextLine());
+        System.out.println("Qual é o ano de nascimento dele?");
+        obj1.setDataDeNascimento(ler.nextInt());
+        System.out.println("Qual é a posição em que ele joga?");
+        obj1.setPosicao(ler.nextLine());
+        System.out.println("Qual a nacionalidade dele?");
+        obj1.setNacionalidade(ler.nextLine());
+        System.out.println("Qual é a altura dele?");
+        obj1.setAltura(ler.nextDouble());
+        System.out.println("Qual é o peso dele?");
+        obj1.setPeso(ler.nextDouble());
+        
+        System.out.println("De acordo com as informações fornecidas:");
+        
+        obj1.mostrarResultado();
+        
+    }
     
 }
--- Aula4/src/br/ulbra/aula4/RegistroJogador.java
+++ Aula4/src/br/ulbra/aula4/RegistroJogador.java
@@ -1,14 +1,104 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ulbra.aula4;
 
-/**
- *
- * @author gusta
- */
 public class RegistroJogador {
-    
+
+    //nome, posicao, dataDeNascimento, nacionalidade,altura,peso
+    private String nome;
+    private String posicao;
+    private int dataDeNascimento;
+    private String nacionalidade;
+    private double altura;
+    private double peso;
+
+    public RegistroJogador() {
+
+        this.nome = nome;
+        this.posicao = posicao;
+        this.dataDeNascimento = dataDeNascimento;
+        this.nacionalidade = nacionalidade;
+        this.altura = altura;
+        this.peso = peso;
+
+    }
+
+    public int calcularIdade(int idade) {
+
+        idade = this.dataDeNascimento - 2024;
+
+        return idade;
+    }
+
+    public int calcularAposentadoria(int aposentadoria) {
+        int idade = 0;
+        idade = calcularIdade(idade);
+
+        if (this.posicao.equalsIgnoreCase("atacante")) {
+            aposentadoria = 35 - idade;
+        } else if (this.posicao.equalsIgnoreCase("defesa")) {
+            aposentadoria = 40 - idade;
+        } else if (this.posicao.equalsIgnoreCase("meiocampo") || this.posicao.equalsIgnoreCase("meio-campo") || this.posicao.equalsIgnoreCase("meio campo")) {
+            aposentadoria = 38 - idade;
+        } else if (idade > 40) {
+
+            aposentadoria = 0;
+        }
+
+        return aposentadoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public int getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(int dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void mostrarResultado() {
+
+        System.out.println("Nome: " + this.nome + "\nPosicao: " + this.posicao
+                + "\nData de nascimento: " + this.dataDeNascimento + "\nIdade: " + calcularIdade(this.dataDeNascimento)
+                + "\nNacionalidade: " + this.nacionalidade + "\nPeso: " + this.peso + "\nAltura: " + this.altura);
+    }
+
 }
